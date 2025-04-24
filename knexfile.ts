ffd41fa12cff2a52f45dcc86b743021499a3aae9
theJRodrigues <gbrodrigues154+github@gmail.com>
2025-04-23 23:10:02 -0300

    feat: create knexfile to ts and db connection file

---
+++ knexfile.ts
@@ -0,0 +1,2 @@
+import {config } from './src/infra/dbConection'
+export default config
---
+++ src/infra/dbConection.ts
@@ -0,0 +1,14 @@
+import knexSteup,{ Knex } from "knex";
+export const config: Knex.Config = {
+  client: "sqlite",
+  connection: {
+    filename: "./db/db.db",
+  },
+  useNullAsDefault: true,
+  migrations:{
+    extension: 'ts',
+    directory: './db/migrations',
+
+  }
+}
+export const knex = knexSteup(config)
