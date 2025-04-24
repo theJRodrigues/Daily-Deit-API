62b0517e587f1beaa7215fb39a75e620bb2ae71f
theJRodrigues <gbrodrigues154+github@gmail.com>
2025-04-23 23:06:43 -0300

    feat: install knex and create knex file

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,11 @@
 import fastify from "fastify";
+import {knex} from "./infra/dbConection";
 export const app = fastify();
+
+app.get("/", async (request, reply) => {
+  const test = await knex('sqlite_schema').select('*');
+  return test;
+})
 
 app.listen({ port: 3000 }, () => {
   console.log("Server is running");
