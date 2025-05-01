10de49ff1d4826af1b2fa816506fb8d175180010
theJRodrigues <gbrodrigues154+github@gmail.com>
2025-05-01 20:59:39 -0300

    refactor: add console errors for clarity

--- src/shared/constants/envVariables.ts
+++ src/shared/constants/envVariables.ts
@@ -1,7 +1,19 @@
 import "dotenv/config";
 import z from "zod";
+
 const envSchema = z.object({
   DB_URL: z.string(),
   KNEX_CLIENT: z.string(),
 });
-export const env = envSchema.parse(process.env);
+
+const _env = envSchema.safeParse(process.env);
+if (!_env.success) {
+  const errors = _env.error.format();
+  console.group("Environment errors");
+  console.error("Environment not found");
+  console.error("Errors: \n" + JSON.stringify(errors) + "\n");
+  console.groupEnd();
+  throw new Error("Environment variables not found ");
+}
+
+export const env = _env.data;
