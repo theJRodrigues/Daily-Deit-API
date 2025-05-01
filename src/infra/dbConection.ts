7051991c6cfe2f694a68370e5a2009a1889a656d
theJRodrigues <gbrodrigues154+github@gmail.com>
2025-04-30 21:16:04 -0300

    feat: create infra folder and add dbConection and server files

--- src/infra/dbConection.ts
+++ src/infra/dbConection.ts
@@ -1,14 +1,13 @@
-import knexSteup,{ Knex } from "knex";
+import knexSteup, { Knex } from "knex";
 export const config: Knex.Config = {
   client: "sqlite",
   connection: {
     filename: "./db/db.db",
   },
   useNullAsDefault: true,
-  migrations:{
-    extension: 'ts',
-    directory: './db/migrations',
-
-  }
-}
-export const knex = knexSteup(config)
+  migrations: {
+    extension: "ts",
+    directory: "./db/migrations",
+  },
+};
+export const knex = knexSteup(config);
